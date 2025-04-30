32066f7603aa6b5b0902f12236b725930ec7e55c
Diogo Xavier <diogodsxavier@gmail.com>
2025-04-30 14:00:08 -0300

    Construção do projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,35 @@
+import fundoMarinho from './assets/fundo-marinho.png';
+
+const animais = [
+  { nome: 'Baleia', emoji: '🐋' },
+  { nome: 'Golfinho', emoji: '🐬' },
+  { nome: 'Polvo', emoji: '🐙' },
+  { nome: 'Tartaruga', emoji: '🐢' },
+]
+
 function App() {
   return (
-    <div className="min-h-screen bg-blue-200 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">A vida Marinha 🌊</h1>
+    <div
+      className='min-h-screen bg-cover bg-center flex flex-col items-center justify-center text-white px-4'
+      style={{ backgroundImage: `url(${fundoMarinho})` }}
+    >
+      <h1 className='text-4xl md:text-5x1 font-bold mb-8 drop-shadow-lg text-center'>
+        A vida Marinha 🌊
+      </h1>
+
+      <div className='grid grid-cols-2 md:grid-cols-4 gap-4'>
+        {animais.map((animal) => (
+          <button
+            key={animal.nome}
+            className='bg-white/20 backdrop-blur-md rounded-2xl p-4 text-xl md:text-2xl font-semibold hover:scale-105 transition-all drop-shadow-md'
+          >
+            {animal.emoji} {animal.nome}
+          </button>
+        ))}
+      </div>
+
     </div>
   )
 }
 
-export default App;
+export default App
